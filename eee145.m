43d8492a783f30495ce33acce34397050b3a72bb
Emmanuel Jesus Estallo <erstallo@up.edu.ph>
2021-07-18 01:57:47 +0800

    Update eee145.m
    added question 4 code

--- eee145.m
+++ eee145.m
@@ -10,7 +10,6 @@ K = 4.5;
 mu = 1.25664e-6;
 
 
-
 %%
 %question number 2 
 i = [0.2 0.5 0.8];
@@ -43,6 +42,27 @@ for n = 1:3
     legend('i=0.2', 'i=0.5', 'i=0.8')
 end
 
+%% 
+%question number 4
+i = [0.2 0.5 0.8];
+t1 = 0:0.01:1;
+x_0 = [0.02 0];
+mu = 1.25664e-6;
+dist = 0.004;
+w = 0.02;
+h = 0.04;
+N = 100;
+xrow = (linspace(x_0(1),x_0(2),101))'*1000;
+const = ones(101,1)*(-2*mu*w*h*N*N);
+figure('NumberTitle', 'off', 'Name', 'Question 4');
+for n = 1:3 
+    [t,y] = ode45(@(t,y) calc_ode(t,y,M,D,K,i(n)), t1, x_0);
+    plot(t, ( (const*i(n)) ./ ((y(:,1) + dist*ones(101,1)).^(2)) ) .* y(:,2))
+    ylabel('Voltage across the coil')
+    xlabel('Time (s)')
+    hold on
+    legend('i=0.2', 'i=0.5', 'i=0.8')
+end
 
 %%
 %question number 1
